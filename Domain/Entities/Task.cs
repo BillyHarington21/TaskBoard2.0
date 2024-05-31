9ef41b1a36a6d38e5c44a53f099345fbab186481
CENTURION 1 <kdtkachenko@gmail.com>
2024-05-31 05:21:04 +0300

    Добавьте файлы проекта.

---
+++ Domain/Entities/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public Guid SprintId { get; set; }
+        public Sprint Sprint { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
